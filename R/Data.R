9e2c3384d5782a86054be4bf9aeececab0dff54b
JingyiZhang <jingyi9787@outlook.com>
2020-05-05 13:16:38 -0400

    Upload the data description

---
+++ R/Data.R
@@ -0,0 +1,16 @@
+#' A simple dataset
+#'
+#' This dataset called myData is a set describing the factors impacting Global Temperature
+#'
+#' @format A data frame with 100 rows and 6 variables:
+#' \describe{
+#'   \item{Y}{this Global Temperature}
+#'   \item{X1}{this is CO2 Emission}
+#'   \item{X2}{this is SO4 Emission}
+#'   \item{X3}{this is O2 Emission}
+#'   \item{X4}{this is CH4 Emission}
+#'   \item{X5}{this is H2O Emission}
+#'   ...
+#' }
+#'
+"myData"
